2712a37f2c09c9e7d86ed737e2ac21f306b55899
Luis <101060519+LuisFun@users.noreply.github.com>
2025-08-03 20:40:48 +0900

    feat: update icon handling and remove unused SVG component
    - Added `@iconify-json/simple-icons` and `astro-icon` as dependencies.
    - Replaced custom SVG component with `astro-icon` for icon rendering in various components.
    - Updated `link.astro` to use `astro-icon` for icons, simplifying the icon management.
    - Removed the `svg.astro` component as it is no longer needed.
    - Updated icon references in `work.astro`, `404.astro`, and `index.astro` to use the new icon format.
    - Introduced `icon-types.ts` to define a type for icons used in the project.
    - Added a new SVG file for the custom icon `luisfun`.

---
+++ src/components/icon-types.ts
@@ -0,0 +1,15 @@
+export type IconType =
+  | "luisfun"
+
+  // https://icon-sets.iconify.design/simple-icons/
+  | "simple-icons:github"
+  | "simple-icons:npm"
+  | "simple-icons:discord"
+  | "simple-icons:patreon"
+
+  | "simple-icons:cloudflare"
+  | "simple-icons:astro"
+  | "simple-icons:svelte"
+  | "simple-icons:tailwindcss"
+  | "simple-icons:gatsby"
+  | "simple-icons:hono"
